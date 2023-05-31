4295b9ca1ef46ce1f19d56867f62debda6d26658
John Doty <johnrdoty92@gmail.com>
2023-05-31 20:11:35 +0900

    add speed interpolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import './App.css'
 
-const directions = ['top', 'left', 'right', 'bottom'] as const;
-type Directions = typeof directions[number];
+const DIRECTIONS = ['top', 'left', 'right', 'bottom'] as const;
+type Directions = typeof DIRECTIONS[number];
 
-const moveMap: Record<Directions, [number, number]> = {
+const SPEED = {
+  min: 1,
+  max: 100,
+} as const;
+
+const MOVE_MAP: Record<Directions, [number, number]> = {
   top: [-1, 0],
   right: [0, 1],
   left: [0, -1],
   bottom: [1, 0],
 }
 
-const oppositeDirections: Record<Directions, Directions> = {
+const OPPOSITE_DIRECTIONS: Record<Directions, Directions> = {
   top: "bottom",
   right: "left",
   left: "right",
@@ -21,21 +26,29 @@ const oppositeDirections: Record<Directions, Directions> = {
 const getCellKey = (cell: [number, number]) => cell.join(',');
 
 const getNextCell = ([cell1r, cell1c]: [number, number], direction: Directions): [number, number] => {
-  const [r, c] = moveMap[direction]
+  const [r, c] = MOVE_MAP[direction]
   return [cell1r + r, cell1c + c]
 }
 
 const getNextDirection = (currentCell: [number, number], nextCell: [number, number]): Directions | undefined => {
   const newCoordinates = [nextCell[0] - currentCell[0], nextCell[1] - currentCell[1]]
-  const directionEntry = Object.entries(moveMap).find((moves) => moves[1].join(',') === newCoordinates.join(','))
-  if (!directionEntry || !directions.find((dir) => directionEntry[0] === dir)) return;
+  const directionEntry = Object.entries(MOVE_MAP).find((moves) => moves[1].join(',') === newCoordinates.join(','))
+  if (!directionEntry || !DIRECTIONS.find((dir) => directionEntry[0] === dir)) return;
   return directionEntry[0] as Directions
 }
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
+const interpolateDelay = (speed: number) => {
+  const SLOWEST_SPEED_IN_MS = 750;
+  const FASTEST_SPEED_IN_MS = 10;
+  const increments = (FASTEST_SPEED_IN_MS - SLOWEST_SPEED_IN_MS) / SPEED.max;
+  return SLOWEST_SPEED_IN_MS + (increments * speed)
+}
+
 function App() {
   const [gridDimensions, setGridDimensions] = useState(10);
+  const [speed, setSpeed] = useState(50);
   const ROWS = (Array(gridDimensions)
     .fill(0)
     .map((_, i) => Array(gridDimensions).fill(0).map<[number, number]>((_, j) => ([i, j])))
@@ -50,6 +63,10 @@ function App() {
 
   const handleDimensions: ChangeEventHandler<HTMLInputElement> = ({target}) => {
     setGridDimensions(target.valueAsNumber)
+  }
+
+  const handleSpeed: ChangeEventHandler<HTMLInputElement> = ({target}) => {
+    setSpeed(target.valueAsNumber)
   }
 
   useEffect(() => {
@@ -67,12 +84,12 @@ function App() {
         if (visited.has(currentCellKey)) continue;
         const [r, c] = currentCell
 
-        await sleep(10);
+        await sleep(interpolateDelay(speed));
         const currentCellNode = gridRef.current.childNodes[r].childNodes[c] as HTMLDivElement;
         currentCellNode.classList.add('visited');
         visited.add(currentCellKey)
 
-        const shuffledDirections = [...directions].sort(() => Math.random() - Math.random())
+        const shuffledDirections = [...DIRECTIONS].sort(() => Math.random() - Math.random())
         shuffledDirections.forEach((direction) => {
           const nextCell = getNextCell(currentCell, direction);
           const nextCellKey = getCellKey(nextCell);
@@ -91,20 +108,20 @@ function App() {
         const nextCellDirection = getNextDirection(currentCell, nextCell);
         const nextCellNode = gridRef.current.childNodes[nextCell[0]].childNodes[nextCell[1]] as HTMLDivElement;
         if (!nextCellDirection) { // dead end
-          for (const direction of directions) {
+          for (const direction of DIRECTIONS) {
             // Find previously visited cell and remove borders
             const neighborCell = getNextCell(nextCell, direction);
             const neighborDirection = getNextDirection(nextCell, neighborCell)
             if (!visited.has(getCellKey(neighborCell)) || !neighborDirection) continue;
             nextCellNode.classList.add(neighborDirection)
             const neighborCellNode = gridRef.current.childNodes[neighborCell[0]].childNodes[neighborCell[1]] as HTMLDivElement;
-            neighborCellNode.classList.add(oppositeDirections[neighborDirection])
+            neighborCellNode.classList.add(OPPOSITE_DIRECTIONS[neighborDirection])
             break;
           }
         } else {
           // Continue dfs and remove borders
           currentCellNode.classList.add(nextCellDirection);
-          nextCellNode.classList.add(oppositeDirections[nextCellDirection])
+          nextCellNode.classList.add(OPPOSITE_DIRECTIONS[nextCellDirection])
         }
       }
     })()
@@ -115,19 +132,28 @@ function App() {
       if (!gridRefCopy.current) return;
       gridRefCopy.current.childNodes.forEach(({childNodes}) => {
         childNodes.forEach(cell => {
-          (cell as HTMLDivElement).classList.remove(...directions, 'visited')
+          (cell as HTMLDivElement).classList.remove(...DIRECTIONS, 'visited')
         })
       })
     }
-  }, [gridDimensions])
+  }, [gridDimensions, speed])
 
   return (
     <div className='container'>
       <div className='grid' ref={gridRef}>
         {grid}
       </div>
-      <button className='button' onClick={() => window.location.reload()}>Reset</button>
-      <input type='range' onChange={handleDimensions} value={gridDimensions} min={5} max={20}/>
+      <div className='controls'>
+        <button className='button' onClick={() => window.location.reload()}>Reset</button>
+        <label>
+          Grid Dimensions: {gridDimensions}
+          <input type='range' onChange={handleDimensions} value={gridDimensions} min={5} max={20}/>
+        </label>
+        <label>
+          Speed: {speed}
+          <input type='range' onChange={handleSpeed} value={speed} min={SPEED.min} max={SPEED.max}/>
+        </label>
+      </div>
     </div>
   )
 }
